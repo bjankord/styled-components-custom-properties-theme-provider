0b30cc8eadf6d7f469b8763a88e4b4042d68bdb2
Brett Jankord <brett.jankord@cerner.com>
2018-12-20 05:00:32 -0600

    Updates

---
+++ precache-manifest.8a609ab690e0f92f25fd59ded3cd5dc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37f47ad9212638fecfac",
+    "url": "/styled-components-custom-properties-theme-provider/static/js/runtime~main.37f47ad9.js"
+  },
+  {
+    "revision": "dd3bb1d0ec25539ef871",
+    "url": "/styled-components-custom-properties-theme-provider/static/js/main.dd3bb1d0.chunk.js"
+  },
+  {
+    "revision": "ff076a449c1f9f7fab66",
+    "url": "/styled-components-custom-properties-theme-provider/static/js/1.ff076a44.chunk.js"
+  },
+  {
+    "revision": "dd3bb1d0ec25539ef871",
+    "url": "/styled-components-custom-properties-theme-provider/static/css/main.04ab87af.chunk.css"
+  },
+  {
+    "revision": "b528c30f0ef15f6d921d9e01f6eda977",
+    "url": "/styled-components-custom-properties-theme-provider/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/styled-components-custom-properties-theme-provider/precache-manifest.8a609ab690e0f92f25fd59ded3cd5dc0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/styled-components-custom-properties-theme-provider/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
